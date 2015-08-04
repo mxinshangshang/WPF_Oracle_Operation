fc4322b8df1413aa0067e6374a2d31633d651f83
mxinshangshang <jiangmingxin825@live.cn>
2015-08-04 17:17:33 +0800

    Can find row num
    20150804

--- OracleConn/MainWindow.xaml.cs
+++ OracleConn/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using System.Data;
 using Oracle.ManagedDataAccess.Client;
 using Oracle.ManagedDataAccess.Types;
+using System.Collections.ObjectModel;
+using System.Reflection;
 
 namespace OracleConn
 {
@@ -30,13 +32,13 @@ namespace OracleConn
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string constring = "data source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST="+ServerName.Text+ ")(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=orcl)));user id="+UserName.Text+ ";password="+Passwd.Text+";";
+            string constring = "data source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=" + ServerName.Text + ")(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=orcl)));user id=" + UserName.Text + ";password=" + Passwd.Text + ";";
             OracleConnection conn = new OracleConnection(constring);
-            try 
-            {  
+            try
+            {
                 conn.Open();
                 DataSet ds = new DataSet();
-                string sql = "select * from "+TableName.Text;
+                string sql = "select * from " + TableName.Text;
                 OracleDataAdapter oda = new OracleDataAdapter(sql, conn);
                 oda.Fill(ds);
                 conn.Close();
@@ -56,12 +58,90 @@ namespace OracleConn
                 dataGrid.DataContext = ds.Tables[0].DefaultView;
             }
             catch (Exception ex)
-            {  
-                MessageBox.Show(ex.ToString());
-            }  
-            finally 
-            {  
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
                 conn.Close();
+            }
+        }
+
+        private void DataGrid_TextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            string constring = "data source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=" + ServerName.Text + ")(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=orcl)));user id=" + UserName.Text + ";password=" + Passwd.Text + ";";
+            OracleConnection conn = new OracleConnection(constring);
+            try
+            {
+                int index = dataGrid.SelectedIndex;
+                /*
+                string del = "delete from User_Table where [User_id] = '" + dataGrid.Rows[index].Cells[0].Value.ToString() + "'";
+                conn.Open();
+                string sql = "select * from " + TableName.Text;
+                OracleDataAdapter oda = new OracleDataAdapter(sql, conn);
+                 */
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedCells.Count > 0)
+            {
+                DataGridCell cell = DataGridHelper.GetCell(dataGrid.SelectedCells[0]);
+                this.Title = DataGridHelper.GetRowIndex(cell).ToString();
+            }
+        }
+    }
+
+    public static class DataGridHelper
+    {
+        public static DataGridCell GetCell(DataGridCellInfo dataGridCellInfo)
+        {
+            if (!dataGridCellInfo.IsValid)
+            {
+                return null;
+            }
+
+            var cellContent = dataGridCellInfo.Column.GetCellContent(dataGridCellInfo.Item);
+            if (cellContent != null)
+            {
+                return (DataGridCell)cellContent.Parent;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static int GetRowIndex(DataGridCell dataGridCell)
+        {
+            // Use reflection to get DataGridCell.RowDataItem property value.
+            PropertyInfo rowDataItemProperty = dataGridCell.GetType().GetProperty("RowDataItem", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            DataGrid dataGrid = GetDataGridFromChild(dataGridCell);
+
+            return dataGrid.Items.IndexOf(rowDataItemProperty.GetValue(dataGridCell, null));
+        }
+        public static DataGrid GetDataGridFromChild(DependencyObject dataGridPart)
+        {
+            if (VisualTreeHelper.GetParent(dataGridPart) == null)
+            {
+                throw new NullReferenceException("Control is null.");
+            }
+            if (VisualTreeHelper.GetParent(dataGridPart) is DataGrid)
+            {
+                return (DataGrid)VisualTreeHelper.GetParent(dataGridPart);
+            }
+            else
+            {
+                return GetDataGridFromChild(VisualTreeHelper.GetParent(dataGridPart));
             }
         }
     }
